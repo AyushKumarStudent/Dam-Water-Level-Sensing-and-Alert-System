7448fa820b4d3eba1fea5d4d2aec39f3be363a1a
Ayush Kumar <ayush.kumar.ec28@iilm.edu>
2025-11-06 23:38:15 +0530

    Add water level alert functionality with LEDs and buzzer

--- Circuit_Diagram/Arduino_Code/water_level_alert.ino
+++ Circuit_Diagram/Arduino_Code/water_level_alert.ino
@@ -1,1 +1,57 @@
+#define trigPin 9
+#define echoPin 8
+#define greenLED 4
+#define yellowLED 3
+#define redLED 2
+#define buzzer 7
 
+long duration;
+int distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(greenLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+  pinMode(redLED, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Trigger the ultrasonic pulse
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;  // Convert to cm
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  if (distance > 20) {
+    digitalWrite(greenLED, HIGH);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(redLED, LOW);
+    noTone(buzzer);
+  } 
+  else if (distance <= 20 && distance > 10) {
+    digitalWrite(greenLED, LOW);
+    digitalWrite(yellowLED, HIGH);
+    digitalWrite(redLED, LOW);
+    noTone(buzzer);
+  } 
+  else {
+    digitalWrite(greenLED, LOW);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(redLED, HIGH);
+    tone(buzzer, 1000, 300);  // Beep at 1kHz for 300ms
+    delay(300);               // Short pause between beeps
+  }
+
+  delay(300);
+}
